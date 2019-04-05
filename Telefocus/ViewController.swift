2d8a1aea828bde0f97ae4471c881b67c6d7481d9
Dakota Kim <dakota@cantina.co>
2019-04-05 10:43:17 -0400

    Initial Commit

---
+++ Telefocus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Telefocus
+//
+//  Created by Dakota Kim on 4/5/19.
+//  Copyright © 2019 Dakota Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

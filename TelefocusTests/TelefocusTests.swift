55526aa361567e85ded8a25d0058e6a8f41331f8
Dakota Kim <dakota@cantina.co>
2019-06-02 13:50:23 -0400

    Add test and travis config

---
+++ TelefocusTests/TelefocusTests.swift
@@ -0,0 +1,25 @@
+//
+//  TelefocusTests.swift
+//  TelefocusTests
+//
+//  Created by Dakota Kim on 6/2/19.
+//  Copyright © 2019 Dakota Kim. All rights reserved.
+//
+
+import XCTest
+
+class TelefocusTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_alwaysPasses() {
+        XCTAssert(true)
+    }
+
+}
